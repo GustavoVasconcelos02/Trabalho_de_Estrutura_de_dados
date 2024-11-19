848c90da713429e6ec04eec65b0883a844f568d9
Robert Coenga <rcoenga@inovvati.com.br>
2024-11-19 16:14:54 -0400

    add: estrutura da trie

---
+++ src/trie/TrieNode.java
@@ -0,0 +1,28 @@
+package src.trie;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import src.archive.LinkedList;
+
+public class TrieNode {
+	
+    Map<Character, TrieNode> children;
+    LinkedList<String> archive;
+    boolean isEndOfWord;
+
+    // Construtor padrão
+    public TrieNode() {
+        this.children = new HashMap<>();
+        this.archive = new LinkedList<>();
+        this.isEndOfWord = false;
+    }
+
+    // Construtor para definir explicitamente o estado de `isEndOfWord`
+    public TrieNode(boolean isEndOfWord) {
+        this.children = new HashMap<>();
+        this.archive= new LinkedList<>();
+        this.isEndOfWord = isEndOfWord;
+    }
+	
+}
